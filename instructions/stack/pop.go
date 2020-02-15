856e30b28f4a404a5f72ffa93b7f20ccfed07bc2
lanboys <lan_bing2013@163.com>
2020-02-15 10:30:39 +0800

    ch06

--- instructions/stack/pop.go
+++ instructions/stack/pop.go
@@ -1,18 +1,11 @@
 package stack
 
-import "jvmgo/ch05/instructions/base"
-import "jvmgo/ch05/rtda"
+import "jvmgo/ch06/instructions/base"
+import "jvmgo/ch06/rtda"
 
 // Pop the top operand stack value
 type POP struct{ base.NoOperandsInstruction }
 
-/*
-bottom -> top
-[...][c][b][a]
-            |
-            V
-[...][c][b]
-*/
 func (self *POP) Execute(frame *rtda.Frame) {
 	stack := frame.OperandStack()
 	stack.PopSlot()
@@ -21,13 +14,6 @@ func (self *POP) Execute(frame *rtda.Frame) {
 // Pop the top one or two operand stack values
 type POP2 struct{ base.NoOperandsInstruction }
 
-/*
-bottom -> top
-[...][c][b][a]
-         |  |
-         V  V
-[...][c]
-*/
 func (self *POP2) Execute(frame *rtda.Frame) {
 	stack := frame.OperandStack()
 	stack.PopSlot()
